c87ef4348e2b0769dcc5361d1420837f86684068
Khazhinov Vladislav <khazhinov@gmail.com>
2023-02-09 15:04:44 +0300

    test new media type with form-data

---
+++ src/Generator/Objects/MediaTypeWithFormData.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+namespace Khazhinov\LaravelFlyDocs\Generator\Objects;
+
+use GoldSpecDigital\ObjectOrientedOAS\Objects\MediaType;
+
+class MediaTypeWithFormData extends MediaType
+{
+    const MEDIA_TYPE_FORM_DATA = 'multipart/form-data';
+
+    /**
+     * @param string|null $objectId
+     * @return static
+     */
+    public static function formData(string $objectId = null): self
+    {
+        return static::create($objectId)
+            ->mediaType(static::MEDIA_TYPE_FORM_DATA);
+    }
+}
